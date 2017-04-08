7b23d764b4d9070db8f02ee9eb8c35ebd7e52db2
Merouane Atig <merwan@patem.net>
2017-04-08 23:15:39 +0200

    Add main character sprite

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
+function Hero(game, x, y) {
+  Phaser.Sprite.call(this, game, x, y, 'hero');
+  this.anchor.set(0.5, 0.5);
+}
+
+Hero.prototype = Object.create(Phaser.Sprite.prototype);
+Hero.prototype.constructor = Hero;
+
 PlayState = {};
 
 PlayState.preload = function() {
   this.game.load.json('level:1', 'data/level01.json');
+
   this.game.load.image('background', 'images/background.png');
+
   this.game.load.image('ground', 'images/ground.png');
   this.game.load.image('grass:8x1', 'images/grass_8x1.png');
   this.game.load.image('grass:6x1', 'images/grass_6x1.png');
   this.game.load.image('grass:4x1', 'images/grass_4x1.png');
   this.game.load.image('grass:2x1', 'images/grass_2x1.png');
   this.game.load.image('grass:1x1', 'images/grass_1x1.png');
+
+  this.game.load.image('hero', 'images/hero_stopped.png');
 };
 
 PlayState.create = function() {
@@ -18,10 +30,16 @@ PlayState.create = function() {
 
 PlayState._loadLevel = function(data) {
   data.platforms.forEach(this._spawnPlatform, this);
+  this._spawnCharaters({ hero: data.hero });
 };
 
 PlayState._spawnPlatform = function(platform) {
   this.game.add.sprite(platform.x, platform.y, platform.image);
+};
+
+PlayState._spawnCharaters = function(data) {
+  this.hero = new Hero(this.game, data.hero.x, data.hero.y);
+  this.game.add.existing(this.hero);
 };
 
 window.onload = function() {
